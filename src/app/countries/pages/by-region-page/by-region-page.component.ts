c6a0997d934f9629e6b91d1f77124b6426959f85
BritanyTorrico <torricobritany2@gmail.com>
2023-10-13 15:39:28 -0400

    app: version 2

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 
 import { CountriesService } from '../../services/country.service';
+import { Region } from '../../interfaces/region.type';
+
+
 
 @Component({
   selector: 'countries-by-region-page',
@@ -9,12 +12,23 @@ import { CountriesService } from '../../services/country.service';
   styles: [
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit{
+
+public regions: Region[]=['Africa','Americas','Asia','Europe','Oceania'];
+public selectedRegion?: Region;
 public countries: Country[] =[];
+public isLoading: boolean = false;
+
+
 constructor(private countriesService: CountriesService){}
+  ngOnInit(): void {
+    this.countries=this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion=this.countriesService.cacheStore.byRegion.region;
+  }
 
 
-searchByRegion(region :string):void{
+searchByRegion(region : Region):void{
+  this.selectedRegion=region;
   this.countriesService.searchRegion(region)
   .subscribe((countries)=> {
     this.countries=countries;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,22 +6,25 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { LoadingSpinerComponent } from './components/loading-spiner/loading-spiner.component';
 
 @NgModule({
   declarations: [
     AboutPagesComponent,
     ContactPagesComponent,
     HomePagesComponent,
-    SidebarComponent,
+    LoadingSpinerComponent,
     SearchBoxComponent,
+    SidebarComponent,
   ],
   imports: [CommonModule, RouterModule],
   exports: [
     AboutPagesComponent,
     ContactPagesComponent,
     HomePagesComponent,
+    LoadingSpinerComponent,
+    SearchBoxComponent,
     SidebarComponent,
-    SearchBoxComponent
   ],
 })
 export class SharedModule {}
